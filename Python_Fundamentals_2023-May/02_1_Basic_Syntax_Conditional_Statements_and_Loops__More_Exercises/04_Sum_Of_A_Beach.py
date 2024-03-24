f5b565634566575ded97fcd12cee4e473cdcebdc
Erj <Erjo82@users.noreply.github.com>
2024-03-24 20:55:33 +0200

    update

--- Python_Fundamentals_2023-May/02_1_Basic_Syntax_Conditional_Statements_and_Loops__More_Exercises/04_Sum_Of_A_Beach.py
+++ Python_Fundamentals_2023-May/02_1_Basic_Syntax_Conditional_Statements_and_Loops__More_Exercises/04_Sum_Of_A_Beach.py
@@ -1,29 +1,21 @@
 word = input().lower()
 find_word = 0
-is_water = 'water'
-is_sand = 'sand'
-is_sun = 'sun'
-is_fish = 'fish'
-
-for index, sand in enumerate(word): # ------ sand -----------
+for index, sand in enumerate(word): # ------ sand & sun -----------
     if sand == 's':
-        find_sand = word[index : index + len(is_sand)]
-        if find_sand == is_sand:
+        find_sand = word[index : index + len('sand')]
+        find_sun = word[index: index + len('sun')]
+        if find_sand == 'sand':
             find_word += 1
-for index2, sun in enumerate(word): # ------ sun -----------
-    if sun == 's':
-        find_sun = word[index2 : index2 + len(is_sun)]
-        if find_sun == is_sun:
+        elif find_sun == 'sun':
             find_word += 1
 for index3, water in enumerate(word): # ------ water -----------
     if water == 'w':
-        find_water = word[index3 : index3 + len(is_water)]
-        if find_water == is_water:
+        find_water = word[index3 : index3 + len('water')]
+        if find_water == 'water':
             find_word += 1
 for index4, fish in enumerate(word): # ------ fish -----------
     if fish == 'f':
-        find_fish = word[index4 : index4 + len(is_fish)]
-        if find_fish == is_fish:
+        find_fish = word[index4 : index4 + len('fish')]
+        if find_fish == 'fish':
             find_word += 1
-
 print(find_word)
